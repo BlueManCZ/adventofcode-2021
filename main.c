f439ed179d27e08aadce9c4f6273d89192bdc898
BlueManCZ <ivo97@centrum.cz>
2021-12-02 18:57:29 +0100

    Rewrite with one printf

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 #include "solutions/day_02.c"
 
 int main(void) {
-    printf("Advent of code 2021 solutions:\n\n");
-    printf("Day 1:\n");
-    printf("  a) %i\n", day1a());
-    printf("  b) %i\n", day1b());
-    printf("\nDay 2:\n");
-    printf("  a) %li\n", day2a());
-    printf("  b) %li\n", day2b());
+    printf("Advent of code 2021 solutions:\n"
+           "\nDay 1:\n"
+           "  a) %i\n"
+           "  b) %i\n"
+           "\nDay 2:\n"
+           "  a) %li\n"
+           "  b) %li\n",
+           day1a(), day1b(),
+           day2a(), day2b()
+    );
 }
