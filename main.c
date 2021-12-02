062a7debad580ae9f80edfaefdebb74eff1db213
BlueManCZ <ivo97@centrum.cz>
2021-12-02 18:43:31 +0100

    Day 2 solved

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include "solutions/day_01.c"
+#include "solutions/day_02.c"
 
 int main(void) {
     printf("Advent of code 2021 solutions:\n\n");
     printf("Day 1:\n");
     printf("  a) %i\n", day1a());
     printf("  b) %i\n", day1b());
+    printf("\nDay 2:\n");
+    printf("  a) %li\n", day2a());
+    printf("  b) %li\n", day2b());
 }
---
+++ solutions/day_02.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long day2a() {
+    char * line = NULL;
+    size_t len = 0;
+    FILE * fp = fopen("../inputs/day_02.txt", "r");
+    ssize_t length;
+
+    long distance = 0;
+    long depth = 0;
+
+    while ((length = getline(&line, &len, fp)) != -1) {
+        switch (length) {
+            case 5: { // Up
+                depth -= strtol(line + 3, NULL, 10);
+                break;
+            }
+            case 7: { // Down
+                depth += strtol(line + 5, NULL, 10);
+                break;
+            }
+            default: { // Forward
+                distance += strtol(line + 8, NULL, 10);
+            }
+        }
+    }
+
+    fclose(fp);
+    return depth * distance;
+}
+
+long day2b() {
+    char * line = NULL;
+    size_t len = 0;
+    FILE * fp = fopen("../inputs/day_02.txt", "r");
+    ssize_t length;
+
+    long distance = 0;
+    long depth = 0;
+    long aim = 0;
+
+    while ((length = getline(&line, &len, fp)) != -1) {
+        switch (length) {
+            case 5: { // Up
+                aim -= strtol(line + 3, NULL, 10);
+                break;
+            }
+            case 7: { // Down
+                aim += strtol(line + 5, NULL, 10);
+                break;
+            }
+            default: { // Forward
+                long value = strtol(line + 8, NULL, 10);
+                distance += value;
+                depth += aim * value;
+            }
+        }
+    }
+
+    fclose(fp);
+    return depth * distance;
+}
